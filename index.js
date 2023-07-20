b33b7cb6d82274467ad14be6d34dcfbee6363bba
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-07-20 21:08:49 +0530

    best

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
+// index.js
+
 const express=require("express");
 const bodyParser=require("body-parser");
 const mongoose=require("mongoose");
 const multer=require("multer");
 const dotenv=require("dotenv");
-const path = require("path");
+const path=require("path");
 
 const app=express();
 app.set("view engine", "ejs");
-
-const staticPath = path.join(__dirname, "public");
-app.use(express.static(staticPath));
 app.use(bodyParser.urlencoded({ extended: true }));
+const staticPath=path.join(__dirname, "public");
+app.use(express.static(staticPath));
 dotenv.config();
+
+const itemSchema=new mongoose.Schema({
+    name: String,
+    cname: String,
+    loc: String,
+    pic: String,
+});
+const Item=mongoose.model("Item", itemSchema);
 
 main().catch((err) => console.log(err));
 
 async function main() {
-
-
     await mongoose.connect(process.env.MONGO_URI);
 
     const storage=multer.diskStorage({
@@ -25,51 +32,50 @@ async function main() {
             cb(null, "public/uploads");
         },
         filename: function (req, file, cb) {
-            cb(null, Date.now()+"-"+file.originalname); // Generate a unique filename for the uploaded file
+            cb(null, Date.now()+"-"+file.originalname);
         },
     });
     const upload=multer({ storage: storage });
 
-    const itemSchema=new mongoose.Schema({
-        name: String,
-        cname: String,
-        loc: String,
-        pic: String
-    });
-    const Item=mongoose.model("Item", itemSchema);
+
 
     const itemsCount=await Item.countDocuments();
     if (itemsCount===0) {
-        const item1=new Item({ name: "Aanchal", cname: "LPU", loc: "Punjab", pic: "" });
+        const item1=new Item({
+            name: "Aanchal",
+            cname: "LPU",
+            loc: "Punjab",
+            pic: "",
+        });
         await item1.save();
     }
 
     app.get("/", function (req, res) {
         Item.find().then((data) => {
-            const items=data.map(item => ({
+            const items=data.map((item) => ({
                 name: item.name,
                 cname: item.cname,
                 loc: item.loc,
-                pic: item.pic
+                pic: item.pic,
             }));
             res.render("index", { items: JSON.stringify(items) });
         });
     });
 
     app.post("/", upload.single("pic"), function (req, res) {
-
         const itemname=req.body.name;
         const itemcname=req.body.cname;
         const itemloc=req.body.loc;
-        const itempic=req.file? req.file.filename:""; // Get the filename of the uploaded image
+        const itempic=req.file? req.file.filename:"";
 
         const item4=new Item({ name: itemname, cname: itemcname, loc: itemloc, pic: itempic });
         item4.save();
         res.redirect("/");
     });
 
-    const port=process.env.PORT||3000;
-    app.listen(port, function () {
-        console.log("success");
-    });
+    return app;
 }
+
+const server=main();
+
+module.exports=server;
