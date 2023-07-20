e961cbd8393cc6d5bf67ba3d3ad0d177c82eae49
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-07-20 14:29:45 +0530

    s

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ main().catch((err) => console.log(err));
 async function main() {
 
 
-    await mongoose.connect("mongodb://0.0.0.0:27017/id");
+    await mongoose.connect("mongodb+srv://Aanchall:Aanchal123@cluster0.zqpnceb.mongodb.net/id");
 
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
